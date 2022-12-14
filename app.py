cad5d8bcd24bc2d890d16a7a2611a4923f0c1064
ABDOUL AZIZ NDIAYE <a.abdoul.ndiaye@gmail.com>
2022-12-14 09:11:51 +0000

    Initialisation du projet
    Création de app.py et de la base de données sur SqlLite

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    identifiant = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.String(255), nullable=False)
+    depense = db.relationship('service', backref='depense')
+    recette = db.relationship('service', backref='recette')
+
+
+class Depense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    plan_compta = db.Column(db.Integer, nullable=False)
+    beneficiaire = db.Column(db.Integer, nullable=False)
+    montant = db.Column(db.Integer, nullable=False)
+    motif = db.Column(db.String(255), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+
+
+
+class Recette(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    plan_compta = db.Column(db.Integer, nullable=False)
+    montant = db.Column(db.Integer, nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+
+
+
+@app.route("/")
+def home():
+    test  = 5
+    return test
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(port = 300, debug=True)
