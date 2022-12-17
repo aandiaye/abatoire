7cb4f8d874296446b5b1636bed58aebbfdaf38b0
blandineruth <ayiteruth004@gmail.com>
2022-12-17 13:49:31 +0100

    modifications  au niveau du service_depense et service

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 # initialize the app with the extension
 db.init_app(app)
 
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     identifiant = db.Column(db.String(255), nullable=False)
@@ -31,7 +32,6 @@ class Depense(db.Model):
     service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
 
 
-
 class Recette(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     plan_compta = db.Column(db.Integer, nullable=False)
@@ -39,19 +39,32 @@ class Recette(db.Model):
     service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
 
 
-
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/form")
-def form():
-    return render_template('pages/form.html')
-@app.route("/data")
-def data():
-    return render_template('pages/data.html')
+
+@app.route("/service")
+def service():
+    return render_template('pages/service.html')
+
+
+@app.route("/form_depense")
+def form_depense():
+    return render_template('pages/form_depense.html')
+
+
+@app.route("/form_recette")
+def form_recette():
+    return render_template('pages/form_recette.html')
+
+
+@app.route("/service_depense")
+def service_depense():
+    return render_template('pages/service_depense.html')
+
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(port = 300, debug=True)
+    app.run(debug=True)
