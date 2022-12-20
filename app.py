73a401b26d4a628d8f8d43660a4bde2859215b1d
TRAORE Abdoul-Aziz <traoreabdoulaziz70@gmail.com>
2022-12-20 19:48:06 +0000

    Ajout fichiers loyers

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, flash
-from flask import  request
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
 app = Flask(__name__)
-
+app.app_context()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 # initialize the app with the extension
 db.init_app(app)
 
@@ -15,6 +15,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     identifiant = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
 
@@ -33,7 +34,13 @@ class Depense(db.Model):
     beneficiaire = db.Column(db.Integer, nullable=False)
     montant = db.Column(db.Integer, nullable=False)
     motif = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+    def __init__(self, plan_compta,beneficiaire,montant,motif,date):
+        self.plan_compta = plan_compta
+        self.beneficiaire = beneficiaire
+        self.montant = montant
+        self.motif = motif
 
 
 
@@ -41,20 +48,8 @@ class Recette(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     plan_compta = db.Column(db.Integer, nullable=False)
     montant = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
-
-
-@app.route("/add_service", methods=["GET", "POST"])
-def add_service():
-    if request.method == "POST":
-        service = Service(
-            service=request.form["firstname"])
-        db.session.add(service)
-        db.session.commit()
-        flash("You are registered and can now login", "success")
-        # return redirect(url_for('login'))
-    else:
-        flash("user already existed, please login or contact admin", "danger")
 
 
 @app.route("/")
@@ -65,19 +60,33 @@ def home():
 def form():
     return render_template('pages/form.html')
 
+
 @app.route("/service")
 def service():
     return render_template('pages/service.html')
 
-@app.route("/form_depense")
+@app.route("/form_depense",methods=['GET', 'POST'])
 def form_depense():
-    return render_template('pages/form_depense.html')
+    if request.method=="POST":
+        plan_compta= request.form["plan_compta"]
+        beneficiaire= request.form["beneficiaire"]
+        montant= request.form["montant"]
+        motif = request.form["motif"]
+        depense_form=Depense(plan_compta=plan_compta, beneficiaire=beneficiaire, montant=montant, motif=motif)
+        try:
+            db.session.add(depense_form)
+            db.session.commit()
+            return redirect('/')
+        except Exception:
+            return "Une erreure s'est produite"
+    else:
+        return render_template('pages/form_depense.html')
 
 @app.route("/form_recette")
 def form_recette():
     return render_template('pages/form_recette.html')
 
-@app.route("/depense")
+@app.route("/depense",methods=['GET', 'POST'])
 def service_depense():
     return render_template('pages/tab_depense.html')
 
@@ -103,6 +112,23 @@ def generale():
     return render_template('pages/generale.html')
 
 '''Les pages services'''
+
+'''Les pages loyer'''
+@app.route("/list_recette")
+def list_recette():
+    return render_template('pages/loyer/list_recette.html')
+
+@app.route("/list_depense")
+def list_depense():
+    return render_template('pages/loyer/list_depense.html')
+
+@app.route("/loyer_form_depense")
+def loyer_form_depense():
+    return render_template('pages/loyer/loyer_form_depense.html')
+
+@app.route("/loyer_form_recette")
+def loyer_form_recette():
+    return render_template('pages/loyer/loyer_form_recette.html')
 
 
 if __name__ == '__main__':
